9c4f32ed8585cfa612745cfcc1fea7a15f0505c6
Debbylight <hansonnolisa@gmail.com>
2023-01-09 00:23:19 -0800

    Static libraries

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strpbrk - Entry point
  * @s: input
@@ -18,5 +17,7 @@ char *_strpbrk(char *s, char *accept)
 			if (*s == accept[k])
 				return (s);
 		}
+		s++;
 	}
+	return ('\0');
 }
